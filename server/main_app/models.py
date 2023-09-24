d9ecd66082e16d6b3003fa8fa6658349764c6b1e
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-24 05:30:21 -0400

    Serves base task object

--- server/main_app/models.py
+++ server/main_app/models.py
@@ -1,3 +1,12 @@
+import django
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+class Task(models.Model):
+  name = models.CharField(max_length=100)
+  startTime = models.DateTimeField()
+  endTime = models.DateTimeField()
+  duration = models.IntegerField()
--- server/main_app/views.py
+++ server/main_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Task
+
+def tasks_index(request):
+  tasks = Task.objects.values()
+  data = list(tasks)  
+  return JsonResponse(data, safe=False) 
