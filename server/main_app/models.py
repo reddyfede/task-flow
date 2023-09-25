7b50f82d8b7fa198f4e8292b33a52c12dc02218b
reddy <reddyfede@gmail.com>
2023-09-25 16:07:15 +0200

    update models to allow blank values

--- server/main_app/models.py
+++ server/main_app/models.py
@@ -2,112 +2,119 @@ from django.db import models
 
 # Create your models here.
 DAYS = (
-  (0, 'Monday'),
-  (1, 'Tuesday'),
-  (2, 'Wednesday'),
-  (3, 'Thursday'),
-  (4, 'Friday'),
-  (5, 'Saturday'),
-  (6, 'Sunday'),
+    (0, "Monday"),
+    (1, "Tuesday"),
+    (2, "Wednesday"),
+    (3, "Thursday"),
+    (4, "Friday"),
+    (5, "Saturday"),
+    (6, "Sunday"),
 )
 
 ROLES = (
-    ('M', 'Manager'),
-    ('E', 'Employee')
+    ("M", "Manager"),
+    ("E", "Employee"),
 )
 
-LOGS_TYPE = (        #TODO: add choices
-    ('ast', 'Actual Start Time'),
-    ('aet', 'Actual End Time'),
+LOGS_TYPE = (                      
+    ("TS", "Task Started"),
+    ("TP", "Task Paused"),
+    ("TI", "Task With Issue"),
+    ("TR", "Task Restarted"),
+    ("TE", "Task Ended"),
 )
+
 
 class Team(models.Model):
     name = models.CharField(max_length=50)
+
     def __str__(self):
-        return f'{self.name} - {self.id}'
-    
+        return f"{self.name} - ({self.id})"
+
+
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     role = models.CharField(
         max_length=1,
         choices=ROLES,
-        default = None,
-        null = True,
-        )
+        default=None,
+        null=True,
+    )
     team = models.ForeignKey(
         Team,
-        on_delete = models.CASCADE,
-        default = None,
-        null = True,
+        on_delete=models.CASCADE,
+        default=None,
+        blank=True,
+        null=True,
     )
+
     def __str__(self):
-        return f'{self.name} - {self.role} - {self.id}'
+        return f"{self.first_name} {self.last_name} - {self.role} - {self.team} - ({self.id})"
+
 
 class Availability(models.Model):
     day = models.IntegerField(
-        choices = DAYS,
+        choices=DAYS,
     )
     pre_lunch_start = models.TimeField()
     pre_lunch_end = models.TimeField()
     post_lunch_start = models.TimeField(
-        default = None,
-        null = True,
+        default=None,
+        blank=True,
+        null=True,
     )
     post_lunch_end = models.TimeField(
-        default = None,
-        null = True,
+        default=None,
+        blank=True,
+        null=True,
     )
-    user = models.ForeignKey(
-        User,
-        on_delete = models.CASCADE
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_day_display()} - {self.user.first_name} {self.user.last_name} - ({self.id})"
+
 
 class Task(models.Model):
     name = models.CharField(max_length=50)
     duration = models.IntegerField()
     start_datetime = models.DateTimeField(
-        default = None,
-        null = True,
+        default=None,
+        blank=True,
+        null=True,
     )
     end_datetime = models.DateTimeField(
-        default = None,
-        null = True,
+        default=None,
+        blank=True,
+        null=True,
     )
-    team = models.ForeignKey(
-        Team,
-        on_delete = models.CASCADE
-    )
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
     user = models.ForeignKey(
         User,
-        on_delete = models.CASCADE,
-        default = None,
-        null = True,
+        on_delete=models.CASCADE,
+        default=None,
+        blank=True,
+        null=True,
     )
+
     def __str__(self):
-        return f'{self.name} - {self.id}'
+        return f"{self.name} - {self.duration} - {self.team.name} - ({self.id})"
+
 
 class TaskLog(models.Model):
-    description = models.CharField(
-        max_length=3,
-        choices = LOGS_TYPE    
-        ) 
+    description = models.CharField(max_length=2, choices=LOGS_TYPE)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField(
-        default = None,
-        null = True,
+        default=None,
+        blank=True,
+        null=True,
     )
     duration = models.IntegerField(
-        default = None,
-        null = True,
+        default=None,
+        blank=True,
+        null=True,
     )
-    task = models.ForeignKey(
-        Task,
-        on_delete = models.CASCADE
-    )
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'{self.name} - {self.id}'
-    
-
-
-
+        return f"{self.get_description_display()} - {self.task.name} - ({self.id})"
