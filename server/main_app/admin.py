55f6c5d66eab08125210bbb89888ad0bbcc98f32
reddy <reddyfede@gmail.com>
2023-09-25 15:22:04 +0200

    create Team,User,Availability,Task,TaskLog models

--- server/main_app/admin.py
+++ server/main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Team, User, Availability, Task, TaskLog
 
 # Register your models here.
+
+admin.site.register(Team)
+admin.site.register(User)
+admin.site.register(Availability)
+admin.site.register(Task)
+admin.site.register(TaskLog)
--- server/main_app/models.py
+++ server/main_app/models.py
@@ -1,3 +1,110 @@
 from django.db import models
 
 # Create your models here.
+DAYS = (
+  (0, 'Monday'),
+  (1, 'Tuesday'),
+  (2, 'Wednesday'),
+  (3, 'Thursday'),
+  (4, 'Friday'),
+  (5, 'Saturday'),
+  (6, 'Sunday'),
+)
+
+ROLES = (
+    ('M', 'Manager'),
+    ('E', 'Employee')
+)
+
+LOGS_TYPE = (
+    ('ast', 'Actual Start Time'),
+    ('aet', 'Actual End Time'),
+
+)
+
+class Team(models.Model):
+    name = models.CharField(max_lenght=50)
+    def __str__(self):
+        return f'{self.name} - {self.id}'
+    
+class User(models.Model):
+    first_name = models.CharField(max_lenght=50)
+    last_name = models.CharField(max_lenght=50)
+    role = models.CharField(
+        max_lenght=1,
+        choices=ROLES
+        )
+    team = models.ForeignKey(
+        Team,
+        on_delete = models.CASCADE
+    )
+    def __str__(self):
+        return f'{self.name} - {self.role} - {self.id}'
+
+class Availability(models.Model):
+    day = models.IntegerField(
+        choices = DAYS,
+    )
+    pre_lunch_start = models.TimeField()
+    pre_lunch_end = models.TimeField()
+    post_lunch_start = models.TimeField(
+        default = None,
+        null = True,
+    )
+    post_lunch_end = models.TimeField(
+        default = None,
+        null = True,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE
+    )
+
+class Task(models.Model):
+    name = models.CharField(max_lenght=50)
+    duration = models.IntegerField()
+    start_datetime = models.DateTimeField(
+        default = None,
+        null = True,
+    )
+    end_datetime = models.DateTimeField(
+        default = None,
+        null = True,
+    )
+    team = models.ForeignKey(
+        Team,
+        on_delete = models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        default = None,
+        null = True,
+    )
+    def __str__(self):
+        return f'{self.name} - {self.id}'
+
+class TaskLog(models.Model):
+    description = models.CharField(
+        max_lenght=50
+        #choices = ...    TODO: add choices
+        ) 
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField(
+        default = None,
+        null = True,
+    )
+    duration = models.IntegerField(
+        default = None,
+        null = True,
+    )
+    task = models.ForeignKey(
+        Task,
+        on_delete = models.CASCADE
+    )
+    def __str__(self):
+        return f'{self.name} - {self.id}'
+    
+
+
+
