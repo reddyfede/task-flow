437b046fd72ecde5ded5597138630afb058329d2
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-26 05:42:27 -0400

    Init auth

--- server/main_app/views.py
+++ server/main_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+@api_view(['POST'])
+def signup(request):
+  serializer = UserSerializer(data = request.data)
+  if serializer.is_valid():
+    serializer.save()
+    user = User.objects.get(username = request.data['username'])
+    token = Token.objects.create(user=user)
+    user.set_password(request.data['password'])
+    user.save()
+    return Response({'token': token.key, 'user': serializer.data})
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+  return Response({})
+
+@api_view(['GET'])
+def test_token(request):
+  return Response({})
