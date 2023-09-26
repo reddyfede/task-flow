c3102c0fe7263c078d7548497402369b460334b5
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-26 14:17:31 -0400

    Adds base login api

--- server/main_app/views.py
+++ server/main_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from .models import AppUser
-
 from .serializers import UserSerializer 
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 @api_view(['POST'])
 def signup(request):
@@ -25,12 +26,18 @@ def signup(request):
 
 @api_view(['POST'])
 def login(request):
-  return Response({})
+  user = get_object_or_404(User, username=request.data['username'])
+  if not user.check_password(request.data['password']):
+      return Response("Username or Password invalid.", status=status.HTTP_404_NOT_FOUND)
+  token, created = Token.objects.get_or_create(user=user)
+  serializer = UserSerializer(user)
+  return Response({'token': token.key, 'user': serializer.data})
 
 @api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request):
-  return Response({})
-# Create your views here.
+  return Response("Token success")
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
