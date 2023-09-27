a5a45bf33842404831978ad7cf9eb24201031c44
reddy <reddyfede@gmail.com>
2023-09-27 12:46:17 +0200

    add redirect to home on logout

--- client/src/components/BtnLogout.jsx
+++ client/src/components/BtnLogout.jsx
@@ -1,18 +1,20 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom"
+import { Link,useNavigate } from "react-router-dom"
 import { UserContext } from "../App";
 
 
 export default function BtnLogout(){
 
+    const navigate = useNavigate();
     const {currUser,setCurrUser} = useContext(UserContext);
 
-    // on click: clear localstorage and clear context
+    // on click: clear localstorage, clear context, redirect to home
     function handleLogout(){
         localStorage.clear("username")
         localStorage.clear("token")
         data = {username:null, token:null}
         setCurrUser(data)
+        navigate("/")
     }
     
     return(
