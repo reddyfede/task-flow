2c3d05a99a33fa4a2a1c3001c4b18b77278ab7d8
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-27 02:02:19 -0400

    Refactors path for view urlpatterns

--- server/main_app/urls.py
+++ server/main_app/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path
 from main_app import views
 from . import views
 
 urlpatterns = [
-    re_path('login', views.login),
-    re_path('signup', views.signup),
-    re_path('test_token', views.test_token),
+    path('login/', views.login),
+    path('signup/', views.signup),
+    path('test_token/', views.test_token),
     path('', views.home, name='home'),
 ]
