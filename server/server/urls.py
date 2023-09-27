f9794189a81b96baa3c77da0b024ea2b7b2ecc64
reddy <reddyfede@gmail.com>
2023-09-27 15:07:23 +0200

    remove unnecessary urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,9 +3,6 @@ from django.urls import include, path, re_path
 from main_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
-    re_path('login', views.login),
-    re_path('signup', views.signup),
-    re_path('test_token', views.test_token)
+    path('admin/', admin.site.urls),
 ]
