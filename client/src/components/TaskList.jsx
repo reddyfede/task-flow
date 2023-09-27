306d95dfe632320b3e1ffba24be7e28181148804
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-27 16:16:59 -0400

    Stub edit and delete handlers

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const TaskList = ({ tasks }) => {
+  const handleEdit = (e) => {
+    console.log(e);
+  };
+  const handleDelete = (e) => {
+    console.log(e);
+  };
+
   return (
     <div>
       <h3>Task List</h3>
@@ -13,6 +20,8 @@ const TaskList = ({ tasks }) => {
                 Name: {t.name} | Due: {t.due_date} | Duration:{' '}
                 {t.planned_duration} | Planned Start: {t.planned_start}
               </p>
+              <button onClick={() => handleEdit(t)}>EDIT</button>
+              <button onClick={() => handleDelete(t)}>DEL</button>
             </div>
           );
         })}
