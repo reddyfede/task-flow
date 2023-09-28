a749ec9d5bd56daf239d1bbee8076a63c1f1c5bc
reddy <reddyfede@gmail.com>
2023-09-28 09:02:09 +0200

    add create team form

--- client/src/components/ManagerTeam.jsx
+++ client/src/components/ManagerTeam.jsx
@@ -10,12 +10,21 @@ export default function ManagerTeam({
   return (
     <div>
       {!userData.teamName ? (
-        <h2>You don't have a team yet. Create a team.</h2>
+        <>
+          <h2>You don't have a team yet.</h2>
+          <h3>Create a team.</h3>
+          <form action=''>
+            <input type='text' />
+            <button>Create Team</button>
+          </form>
+        </>
       ) : (
         <>
           <h2>
             Team Name : {userData.teamName} - TeamId: {userData.teamId}
           </h2>
+          <button>Edit Team Name</button>
+          <button>Delete Team</button>
           <hr />
           <hr />
           <EmployeeList
