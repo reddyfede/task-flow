595a2af11ee06ca025983ce44d1572bb1322ab79
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-27 21:38:59 -0400

    Updates eslint

--- client/src/api/task-service.js
+++ client/src/api/task-service.js
@@ -2,8 +2,8 @@ import * as tasksAPI from './task-api'
 
 export async function getTasks(data){
     try{
-       const resp = await tasksAPI.index(data)
-       return resp
+        const resp = await tasksAPI.index(data);
+        return resp;
     }catch(err){
         return err
     }
